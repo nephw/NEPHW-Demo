88e5dce7f75cb733e14f4746406d23639834ca25
0xMislav <mislav@dev3.sh>
2022-10-09 07:58:38 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BigNumber } from 'ethers';
+import { publishTransactionFlow } from 'nephw-sdk';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nephw-demo';
+
+  txFlowCid: string = ""
+
+  publish() {
+    publishTransactionFlow([
+      {
+        chainId: 80001,
+        tx: {
+          to: "0x0",
+          value: BigNumber.from(12)
+        }
+      }
+    ]).then(res => { this.txFlowCid = res })
+  }
 }
